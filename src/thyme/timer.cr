d89ab35694e9d9a31d8a67c75f4786f196799407
Hugh Bien <hugh@hughbien.com>
2020-04-07 10:31:13 -0500

    Refactor instance vars in Timer

--- src/thyme/timer.cr
+++ src/thyme/timer.cr
@@ -1,18 +1,15 @@
 require "../thyme"
 
 class Thyme::Timer
-  private getter tmux : Tmux
-  private getter start_time : Time
-  private getter end_time : Time
-
+  @tmux : Tmux
   @stop : Bool = false
-  @pause_time : Time | Nil
   @repeat_total : Int32 = 5
+  @end_time : Time
+  @pause_time : Time | Nil
 
   def initialize
     @tmux = Tmux.new
-    @start_time = Time.local
-    @end_time = @start_time + (25*60).seconds
+    @end_time = Time.local + (25*60).seconds
   end
 
   def run
@@ -24,24 +21,7 @@ class Thyme::Timer
       run_single(repeat_index, true) unless repeat_index == @repeat_total
       repeat_index += 1
     end
-    tmux.set_status("")
-  end
-
-  def run_single(repeat_index, on_break = false)
-    return if @stop
-
-    @end_time = Time.local + ((on_break ? 1 : 2) * 5).seconds
-    while Time.local < end_time
-      if @stop
-        tmux.set_status("")
-        return
-      end
-
-      tmux.set_status(
-        Format.status(time_remaining, repeat_index, @repeat_total, on_break)
-      ) unless @pause_time
-      sleep(1)
-    end
+    @tmux.set_status("")
   end
 
   def stop
@@ -58,7 +38,24 @@ class Thyme::Timer
     end
   end
 
+  private def run_single(repeat_index, on_break = false)
+    return if @stop
+
+    @end_time = Time.local + ((on_break ? 1 : 2) * 5).seconds
+    while Time.local < @end_time
+      if @stop
+        @tmux.set_status("")
+        return
+      end
+
+      @tmux.set_status(
+        Format.status(time_remaining, repeat_index, @repeat_total, on_break)
+      ) unless @pause_time
+      sleep(1)
+    end
+  end
+
   private def time_remaining
-    end_time - Time.local
+    @end_time - Time.local
   end
 end
